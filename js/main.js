4cfebc3be753d008ac54df75f9158dab9ca7281a
Mohammed <el-med7@outlook.com>
2020-12-28 14:26:40 +0100

    exo2

---
+++ js/main.js
@@ -0,0 +1,91 @@
+// pb-1
+
+let prenom = 'jojo'
+let age = 55
+
+let person = prenom +' '+age
+console.log(person);
+
+let text = 'J’aime les tartines et j’en mange'
+console.log(text);
+
+let secondText = "4 par jours"
+console.log(secondText)
+
+let num = 5
+console.log(num);
+
+let numDeux = 35
+console.log(numDeux);
+
+let x = num + numDeux;
+console.log(x);
+
+let xText = text +' '+ num
+console.log(xText);
+
+// pb-2
+
+var un = 4
+var deux = '6'
+var result;
+
+if (un > deux){
+
+    result = 'true'
+}else {
+
+    result = false
+}
+console.log(result);
+
+
+if (un < deux){
+
+    result = 'true'
+}else {
+
+    result = false
+}
+console.log(result);
+
+
+if (un == deux){
+
+    result = 'true'
+}else {
+
+    result = false
+}
+console.log(result);
+
+
+if (un === deux){
+
+    result = 'true'
+}else {
+
+    result = false
+}
+console.log(result);
+
+
+if (un != deux){
+
+    result = 'true'
+}else {
+
+    result = false
+}
+console.log(result);
+
+
+
+if (un !== deux){
+
+    result = 'true'
+}else {
+
+    result = false
+}
+console.log(result);
